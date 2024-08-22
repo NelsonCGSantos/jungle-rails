ccabd4f4432e3141c1778f1e9c9eda16aa1908bf
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-08-22 17:05:31 -0600

    Enhancement: Order details page

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,11 @@
 class Order < ApplicationRecord
-  
-  has_many :line_items
+  has_many :line_items, dependent: :destroy
 
   monetize :total_cents, numericality: true
 
   validates :stripe_charge_id, presence: true
 
+  def total_price
+    total_cents / 100.0
+  end
 end
