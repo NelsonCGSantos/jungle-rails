32f45f0fb97de99d3bf180629fe5b12eeef37972
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-08-27 23:01:13 -0600

    add Cypress test for add to cart functionality

--- cypress/e2e/product_details.cy.js
+++ cypress/e2e/product_details.cy.js
@@ -7,10 +7,10 @@ describe("Product Details", () => {
     // Find the first product and click on it
     cy.get(".products article").first().click();
 
-    // Assert that the product detail page is displayed
+    // The product detail page is displayed
     cy.get(".product-detail").should("be.visible");
 
-    // Optionally, verify that the URL changes as expected
+    // Verify that the URL changes as expected
     cy.url().should("include", "/products/");
   });
 });
